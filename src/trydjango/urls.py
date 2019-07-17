0e2d2af58040d13854e55c03a8b12f3d94ab866b
DinhNhoBao <nhobao2000@gmail.com>
2019-07-18 00:29:53 +0700

    backup commit

--- src/trydjango/urls.py
+++ src/trydjango/urls.py
@@ -29,5 +29,5 @@ urlpatterns = [
     url(r'^accounts/', include('allauth.urls')),
     url(r'^$', views.products_list, name = 'home'), #remember to use as_view() to render view
     url(r'^report_bug/$', views.report_bug, name = 'report'),
-    url(r'authentication/', TemplateView.as_view(template_name = 'login/index.html')),
+    url(r'authentication/', TemplateView.as_view(template_name = 'login/index.html'), name = 'authentication'),
 ]
